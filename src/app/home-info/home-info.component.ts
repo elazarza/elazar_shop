bd0a6b938d6f86c98547d7a01c416dbe3f0a8857
elazarza <44997619+elazarza@users.noreply.github.com>
2020-01-15 21:07:42 +0200

    sdf

---
+++ src/app/home-info/home-info.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit,Input } from '@angular/core';
+import { shopService } from '../shop.services'
+
+@Component({
+  selector: 'app-home-info',
+  templateUrl: './home-info.component.html',
+  styleUrls: ['./home-info.component.css']
+})
+export class HomeInfoComponent implements OnInit {
+
+  constructor(public shopService: shopService) { }
+
+  public countProducts = []
+  public countOrders = []
+  @Input() toHomeInfo;
+
+  ngOnInit() {
+    this.shopService.getinfo().subscribe(
+      (res: any) => {
+        let countp:string = res.message[0].countInfo.toString();
+        let counto:string = res.message[1].countInfo.toString();
+        for(let i=0; i<countp.length;i++){
+          this.countProducts[i] = countp[i]
+        }
+        for(let j=0; j<counto.length;j++){
+          this.countOrders[j] = counto[j]
+        }
+      }
+    )
+  }
+
+}
